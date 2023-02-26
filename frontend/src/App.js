2a13ab4470ff75656639edc34f088815f2e5ac74
kwYoohae <gocksdl7979@gmail.com>
2023-02-26 19:51:00 +0900

    feat: backend로 부터 TodoItem Fetch 하는 로직 작성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Todo from "./Todo";
 import {useState} from "react";
-import {List, Paper} from "@mui/material";
+import {Container, List, Paper} from "@mui/material";
 import AddTodo from "./AddTodo";
 
 function App() {
     const [items, setItems] = useState([]);
+
+    const requestOptions = {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+    };
+
+    fetch("http://localhost:8080/todo", requestOptions)
+        .then((response) => response.json())
+        .then((response) => {
+            setItems(response.data);
+        },
+            (error) => {
+
+            }
+        );
 
     const addItem = (item) => {
         item.id = "ID-" + items.length;
@@ -37,8 +52,10 @@ function App() {
 
     return (
         <div className="App">
-            <AddTodo addItem={addItem}/>
-            <div className="TodoList">{todoItems}</div>
+            <Container maxWidth="md">
+                <AddTodo addItem={addItem}/>
+                <div className="TodoList">{todoItems}</div>
+            </Container>
         </div>
     );
 }
