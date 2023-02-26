26ad2973203adf82752aeb64d1aa49f75754702d
kwYoohae <gocksdl7979@gmail.com>
2023-02-26 15:47:50 +0900

    feat: Repository 구현

--- backend/src/main/java/com/example/backend/model/TodoEntity.java
+++ backend/src/main/java/com/example/backend/model/TodoEntity.java
@@ -1,18 +1,24 @@
 package com.example.backend.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Table(name = "Todo")
 public class TodoEntity {
 
   @Id
+  @GeneratedValue(generator = "system-uuid") // 기본 generator
+  @GenericGenerator(name = "system-uuid", strategy = "uuid")
   private String id;
   private String userId;
   private String title;
