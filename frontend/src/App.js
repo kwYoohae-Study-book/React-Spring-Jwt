ecc1599aa3ee40f77b3d3984a8b3fbbcd03681dc
kwYoohae <gocksdl7979@gmail.com>
2023-02-28 19:58:13 +0900

    feat: UI 글리치 해결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Todo from "./Todo";
 import {useEffect, useState} from "react";
-import {Container, List, Paper} from "@mui/material";
+import {AppBar, Button, Container, Grid, List, Paper, Toolbar, Typography} from "@mui/material";
 import AddTodo from "./AddTodo";
-import {call} from "./service/ApiService";
+import {call, signout} from "./service/ApiService";
 
 function App() {
     const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-       call("/todo", "GET", null)
-           .then((response) => setItems(response.data));
+        call("/todo", "GET", null)
+            .then((response) => {
+                    setItems(response.data);
+                    setLoading(false);
+                }
+            )
+        ;
     }, []);
 
     const addItem = (item) => {
@@ -31,19 +37,50 @@ function App() {
     let todoItems = items.length > 0 && (
         <Paper style={{margin: 16}}>
             <List>
-                {
-                    items.map((item) => (<Todo item={item} key={item.id} deleteItem={deleteItem} editItem={editItem}/>))
-                }
+                {items.map((item) => (
+                    <Todo item={item} key={item.id} editItem={editItem} deleteItem={deleteItem}/>
+                ))}
             </List>
         </Paper>
+    );
+
+    let navigationBar = (
+        <AppBar position="static">
+            <Toolbar>
+                <Grid justifyContent="space-between" container>
+                    <Grid item>
+                        <Typography variant="h6">오늘의 할일</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button color="inherit" raised onClick={signout}>
+                            로그아웃
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
     )
 
-    return (
-        <div className="App">
+    let todoListPage = (
+        <div>
+            {navigationBar}
             <Container maxWidth="md">
                 <AddTodo addItem={addItem}/>
                 <div className="TodoList">{todoItems}</div>
             </Container>
+        </div>
+    );
+
+    let loadingPage = <h1>로딩중...</h1>;
+    let content = loadingPage;
+
+    if (!loading) {
+        content = todoListPage;
+    }
+
+    return (
+        <div className="App">
+            {content}
         </div>
     );
 }
